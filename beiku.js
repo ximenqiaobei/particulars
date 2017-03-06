badec55e51828067b39c4b5d23e16ca6268e021b
西门桥北 <ximenqiaobei@foxmail.com>
2017-03-06 12:32:35 +0800

    beiku忘记保存

--- beiku.js
+++ beiku.js
@@ -17,6 +17,13 @@ var bindEvent = function(element, eventName, callback){
     element.addEventListener(eventName, callback)
 }
 
+var bindAll = function(element, eventName, callback){
+    var elements = document.querySelectorAll(element)
+    for (var i = 0; i < elements.length; i++) {
+        var e = elements[i]
+        bindEvent(e, eventName, callback)
+    }
+}
 
 var toggleClass = function(element, className) {
     if (element.classList.contains(className)) {
@@ -24,4 +31,8 @@ var toggleClass = function(element, className) {
     } else {
         element.classList.add(className)
     }
+}
+
+var find = function(element, selector){
+    return element.querySelector(selector)
 }
