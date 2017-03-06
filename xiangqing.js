638aab1dd8e40bcb30818e01ef14dbe20c16f670
西门桥北 <ximenqiaobei@foxmail.com>
2017-03-06 11:32:56 +0800

    多个详情

--- xiangqing.js
+++ xiangqing.js
@@ -5,16 +5,33 @@
 3，在点击按钮时切换内容的显示
 
 */
+// 
+// var bindEventToggle = function(){
+//     var b = e('.menu-toggle')
+//     bindEvent(b, 'click', function(){
+//         // log('按钮绑定成功')
+//         // 找到 mennu-content
+//         var c = e('.menu-content')
+//         // 开关 hide class
+//         toggleClass(c, 'hide')
+//     })
+// }
 
-var bindEventToggle = function(){
-    var b = e('.menu-toggle')
-    bindEvent(b, 'click', function(){
-        // log('按钮绑定成功')
-        // 找到 mennu-content
-        var c = e('.menu-content')
+// bindEventToggle()
+
+
+var bindEventToggles = function(){
+    var selector = '.menu-toggle'
+    bindAll(selector, 'click', function(event){
+        log('按钮绑定成功')
+        // 找到 自己菜单的 mennu-content
+        var fu = event.target.parentElement
+
+        var c = find(fu, '.menu-content')
         // 开关 hide class
         toggleClass(c, 'hide')
     })
 }
 
-bindEventToggle()
+
+bindEventToggles()
